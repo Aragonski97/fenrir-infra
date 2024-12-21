ef84fa2e97e5faa8486efef45d37c3faae9c5e13
lazar.jovanovic <l.jovanovic97@gmail.com>
2024-12-21 14:41:36 +0100

    1. Label changes to node.labels.<tech> == true / false 2. Structure changes from postgres+metabase to postgres and analytics/metabase 3. Addition of deployment control features in docker stacks 4. Airflow changes to env_file, proper loading set up

--- setup.sh
+++ setup.sh
@@ -5,19 +5,28 @@
 export TSC_IP=$(tailscale ip | awk 'FNR == 1 {print}')
 export TSC_IP_BASE=$(tailscale ip | awk 'NR==1' | grep -oP '^\d+\.\d+')
 export FENRIR_DOCKER_NETWORK=$(hostname)-network
+export FENRIR_ROOT_DIR=$(pwd)
 
-docker swarm init --advertise-addr $(echo $TSC_IP)
+# init docker swarm
+docker swarm init --advertise-addr ${TSC_IP}
 
+# docker swarm leader id
 export DOCKER_LEADER_NODE_ID=$(docker node ls | grep Leader | awk '{print $1}')
 
 
-docker node update --label-add orchestration=airflow $(echo $DOCKER_LEADER_NODE_ID)
-docker node update --label-add service=kafka $(echo $DOCKER_LEADER_NODE_ID)
+# update for restraints
+docker node update --label-add kafka=true $DOCKER_LEADER_NODE_ID
+docker node update --label-add dev=true $DOCKER_LEADER_NODE_ID
+docker node update --label-add airflow=true $DOCKER_LEADER_NODE_ID
+docker node update --label-add analytics=true $DOCKER_LEADER_NODE_ID
+docker node update --label-add postgres=true $DOCKER_LEADER_NODE_ID
 
+
+# create network for swarm
 docker network create \
 --driver overlay \
 --subnet="${TSC_IP_BASE}.0.0/16" \
---gateway=$(echo $TSC_IP) \
+--gateway=$TSC_IP \
 --attachable \
 --label layer=core \
 --scope global \
@@ -30,10 +39,16 @@ $(hostname)-network
 # https://stackoverflow.com/a/58670417
 # https://stackoverflow.com/a/58082993 also
 
-docker stack deploy -c ~/.fenrir/kafka/kafka-stack.yml kafka --detach=false
+# deployment
 
-docker stack deploy -c ~/.fenrir/portainer/portainer-stack.yml portainer --detach=false
+docker stack deploy -c ${FENRIR_ROOT_DIR}/kafka/kafka-stack.yml kafka --detach=false
 
-docker stack deploy -c ~/.fenrir/postgres+metabase/postgres-metabase-stack.yml postgres_metabase --detach=false
+docker stack deploy -c ${FENRIR_ROOT_DIR}/portainer/portainer-stack.yml portainer --detach=false
 
-docker stack deploy -c ~/.axiom_infra/fenrir-infrastructure/airflow/airflow-stack.yml airflow --detach=false
+docker stack deploy -c ${FENRIR_ROOT_DIR}/postgres/postgres-stack.yml postgres --detach=false
+
+docker stack deploy -c ${FENRIR_ROOT_DIR}/analytics/metabase-stack.yml metabase --detach=false
+
+docker stack deploy -c ${FENRIR_ROOT_DIR}/airflow/local-executor-airflow-stack.yml airflow --detach=false
+
+docker stack deploy -c ${FENRIR_ROOT_DIR}/devbox/devbox-stack.yml devbox --detach=false
