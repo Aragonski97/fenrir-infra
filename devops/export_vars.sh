92f9e8980191e8eb87c9fcf731fae62313a259a8
lazar.jovanovic <l.jovanovic97@gmail.com>
2024-12-28 18:26:24 +0100

    export_vars.sh added tailscale login

--- devops/export_vars.sh
+++ devops/export_vars.sh
@@ -1,3 +1,5 @@
+sudo tailscale login
+
 export TSC_IP=$(tailscale ip | awk 'FNR == 1 {print}')
 export TSC_IP_BASE=$(tailscale ip | awk 'NR==1' | grep -oP '^\d+\.\d+')
 export FENRIR_DOCKER_NETWORK=$(hostname)-network
